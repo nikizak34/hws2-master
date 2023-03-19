a67f5e6006d72c040fc83a90b309349089ec55e9
nikizak34 <nikita2000.16.11@gmail.com>
2023-03-19 12:12:53 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,27 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            let newState = [...state]
 
-            return state // need to fix
+            return newState.sort((a, b) =>{
+                if(action.payload==='up'){
+                    return a.name.localeCompare(b.name)
+                }else {
+                    return b.name.localeCompare(a.name)
+                }
+
+            })
+
+
+
+            // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload) // need to fix
         }
         default:
             return state
